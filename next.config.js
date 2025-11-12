e41c0d34462e5f164be828844945cdf487325cc7
aakmal101 <aakmal101@gmail.com>
2025-11-12 18:24:03 +0600

    Clean up Next.js config env usage

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['localhost', 'res.cloudinary.com'],
-  },
-  env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
 }
 
